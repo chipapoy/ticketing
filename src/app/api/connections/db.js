1e17324e6df672eeb6d6a9c8813ffb82a096c306
chipapoy <mikdrake13@gmail.com>
2026-04-12 19:33:50 -0400

    add env variable

---
+++ src/app/api/connections/db.js
@@ -0,0 +1,22 @@
+import mysql from 'serverless-mysql';
+
+const db = mysql({
+  config: {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    database: process.env.DB_DATABASE,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS
+  }
+});
+
+export default async function executeQuery({ query, values }) {
+  try {
+    const results = await db.query(query, values);
+    await db.end();
+    return results;
+  } 
+  catch (error) {
+    return { error };
+  }
+}
