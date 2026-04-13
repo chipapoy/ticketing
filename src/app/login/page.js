2d0514f705bd149462cb47b43e81ef02151246f3
chipapoy <mikdrake13@gmail.com>
2026-04-13 02:51:43 -0400

    change username and password input ui

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -4,12 +4,24 @@ import { useState, useEffect } from 'react'
 import { useRouter } from 'next/navigation'
 import { md5 } from 'js-md5';
 import axios from 'axios';
-import { Input, Typography } from "@material-tailwind/react";
+import TextField from '@mui/material/TextField';
 import Image from 'next/image';
 import moment from 'moment';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import Form_renew_password from '@/Component/Forms/Form_renew_password';
+
+import IconButton from '@mui/material/IconButton';
+import Input from '@mui/material/Input';
+import FilledInput from '@mui/material/FilledInput';
+import OutlinedInput from '@mui/material/OutlinedInput';
+import InputLabel from '@mui/material/InputLabel';
+import InputAdornment from '@mui/material/InputAdornment';
+import FormHelperText from '@mui/material/FormHelperText';
+import FormControl from '@mui/material/FormControl';
+import Visibility from '@mui/icons-material/Visibility';
+import VisibilityOff from '@mui/icons-material/VisibilityOff';
+import AccountCircle from '@mui/icons-material/AccountCircle';
 
 const Login = () => {
 
@@ -25,18 +37,32 @@ const Login = () => {
   const [openModal, setOpenModal] = useState(false);
   const [userId, setUserId] = useState(false);
 
+  const [showPassword, setShowPassword] = useState(false);
+
+  const handleClickShowPassword = () => setShowPassword((show) => !show);
+
+  const handleMouseDownPassword = (event) => {
+    event.preventDefault();
+  };
+
+  const handleMouseUpPassword = (event) => {
+    event.preventDefault();
+  };
+
   useEffect(() => {
-    if (localStorage.getItem('id') !== null) {
-      router.push('/dashboard')
+
+    const id = localStorage.getItem('id');
+    if (id && window.location.pathname !== '/dashboard') {
+      router.replace('/dashboard');
     }
   }, []);
 
-  
-  
+
+
   const modalCallback = (data) => {
-    
+
     setOpenModal(data.open)
-    if(data.passwordUpdated){
+    if (data.passwordUpdated) {
       window.location.reload()
     }
   }
@@ -87,15 +113,14 @@ const Login = () => {
             setUserId(data.id);
           }
           else {
+            localStorage.setItem('id', data.id);
+            localStorage.setItem('name', data.name);
+            localStorage.setItem('email_add', data.email_add);
+            localStorage.setItem('username', data.username);
+            localStorage.setItem('role_id', data.role_id);
+            localStorage.setItem('tech_id', data.tech_id);
 
-            if (typeof window !== 'undefined') {
-              localStorage.setItem('id', data.id);
-              localStorage.setItem('name', data.name);
-              localStorage.setItem('email_add', data.email_add);
-              localStorage.setItem('username', data.username);
-              localStorage.setItem('role_id', data.role_id);
-              localStorage.setItem('tech_id', data.tech_id);
-            }
+            // localStorage.setItem('data',JSON.stringify(data))
 
             accessLogsHandler({
               username: data.username,
@@ -176,7 +201,7 @@ const Login = () => {
               alt="The Moment Group"
             />
             Ticketing System
-           
+
           </div>
         </div>
         <div className="mt-10 space-y-6 sm:mx-auto sm:w-full sm:max-w-sm">
@@ -184,32 +209,67 @@ const Login = () => {
             <div>
               <div className="mb-5">
                 <div className="flex flex-col items-end gap-6">
-                  <Input
-                    id="username"
-                    name="username"
-                    label="Username"
-                    type="text"
-                    size="md"
-                    required={true}
-                    // value={user.username}
+                  <FormControl
+                    fullWidth
+                    size="small"
+                    variant="outlined"
                     onChange={handleForm}
-                  />
+                    required
+                  >
+                    <InputLabel htmlFor="outlined-adornment-username">Username</InputLabel>
+                    <OutlinedInput
+                      id="outlined-adornment-username"
+                      name="username"
+                      type="text"
+                      endAdornment={
+                        <InputAdornment position="end">
+                          <IconButton
+                            edge="end"
+                          >
+                            <AccountCircle />
+                          </IconButton>
+                        </InputAdornment>
+                      }
+                      label="Username"
+                    />
+                  </FormControl>
                 </div>
               </div>
             </div>
             <div>
               <div className="mb-5">
                 <div className="flex flex-col items-end gap-6">
-                  <Input
-                    id="password"
-                    name="password"
-                    label="Password"
-                    type="password"
-                    size="md"
-                    required={true}
-                    // value={user.password}
+
+                  <FormControl
+                    fullWidth
+                    size="small"
+                    variant="outlined"
                     onChange={handleForm}
-                  />
+                    required
+                  >
+                    <InputLabel htmlFor="outlined-adornment-password">Password</InputLabel>
+                    <OutlinedInput
+                      id="outlined-adornment-password"
+                      name="password"
+                      type={showPassword ? 'text' : 'password'}
+                      endAdornment={
+                        <InputAdornment position="end">
+                          <IconButton
+                            aria-label={
+                              showPassword ? 'hide the password' : 'display the password'
+                            }
+                            onClick={handleClickShowPassword}
+                            onMouseDown={handleMouseDownPassword}
+                            onMouseUp={handleMouseUpPassword}
+                            edge="end"
+                          >
+                            {showPassword ? <VisibilityOff /> : <Visibility />}
+                          </IconButton>
+                        </InputAdornment>
+                      }
+                      label="Password"
+                    />
+                  </FormControl>
                 </div>
               </div>
             </div>
@@ -230,12 +290,18 @@ const Login = () => {
         </div>
       </div>
       <div>
-        <Form_renew_password isOpen={openModal} modalCallback={modalCallback} userId={userId} />
+        {openModal && (
+          <Form_renew_password
+            isOpen={openModal}
+            modalCallback={modalCallback}
+            userId={userId}
+          />
+        )}
       </div>
       <div>
         <ToastContainer />
       </div>
-      
+
     </div>
   )
 }
